0ffcd375ab0d72633b8b7cbcd1f1721a853ec087
Goran Marković <91391493+goran010@users.noreply.github.com>
2024-05-10 08:52:29 +0200

    added IdentityService

---
+++ src/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService;
+
+public class RegisterViewModel
+{
+    [Required]
+    public string Email { get; set; } = string.Empty; // Initialized to prevent warning
+
+    [Required]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    public string FullName { get; set; } = string.Empty;
+
+    public string? ReturnUrl { get; set; } // Nullable, it's okay if this is null
+
+    public string? Button { get; set; }
+}
